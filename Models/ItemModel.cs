e103fff78a60850fff53beaa176c0f3169e46618
Potlapalli <potlapalliananth@gmail.com>
2021-07-21 23:24:24 -0500

    Add files via upload

---
+++ Models/ItemModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoListWebApp.Models
+{
+    /// <summary>
+    /// Summary description for Item
+    /// </summary>
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+    }
+}
